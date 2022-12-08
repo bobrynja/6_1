306abca14daf650d26948027b533ac303d71d8cb
bobrynja <119498240+bobrynja@users.noreply.github.com>
2022-12-08 17:45:13 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void f1()
+        {
+
+            Console.WriteLine("Работа с одномерным массивом.");
+            Console.Write("Введите количество чисел: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n == 0) Console.WriteLine("Размер массива не может быть равен 0");
+            else
+            {
+                Console.Write("Введите число, с которым будет происходить сравнение остальных чисел: ");
+                double number = Convert.ToDouble(Console.ReadLine());
+                int[] arr = new int[n];
+                Console.WriteLine("Введите числа: ");
+                for (int i = 0; i < n; i++)
+                {
+                    arr[i] = Convert.ToInt32(Console.ReadLine());
+                }
+                for (int i = 0; i < n; i++)
+                {
+                    if (arr[i] < number) arr[i] *= 2;
+                }
+                Console.WriteLine("Вывод результата: ");
+                for (int i = 0; i < n; i++)
+                {
+                    Console.Write(arr[i]);
+                    if (i < arr.Length - 1) Console.Write(", ");
+                }
+            }
+        }
+
+        static void f2()
+        {
+            Console.WriteLine("Работа с двумерным массивом.");
+            Console.WriteLine("Введите размерность массива: ");
+            Console.Write("n = ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("m = ");
+            int m = Convert.ToInt32(Console.ReadLine());
+            if (n <= 0 || m <= 0) Console.WriteLine("Размер массива не может быть равен 0 или быть отрицательным");
+            else
+            {
+                Console.Write("Введите число, с которым будет происходить сравнение остальных чисел: ");
+                double number = Convert.ToDouble(Console.ReadLine());
+                int[,] arr = new int[n, m];
+                Console.WriteLine("Введите числа: ");
+                for (int i = 0; i < n; i++)
+                {
+                    string[] a = Console.ReadLine().Split(' ');
+                    for (int j = 0; j < m; j++)
+                    {
+                        if (Convert.ToInt32(a[j]) < number) arr[i, j] = Convert.ToInt32(a[j]) * 2;
+                        else arr[i, j] = Convert.ToInt32(a[j]);
+                    }
+                }
+                Console.WriteLine("Вывод результата: ");
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < m; j++) Console.Write(arr[i, j] + "  ");
+                    Console.WriteLine();
+                }
+            }
+
+        }
+        static void Main(string[] args)
+        {           
+                try
+                {
+                    f1();
+                    Console.WriteLine();
+                    Console.WriteLine("----------------------------------------");
+                    Console.WriteLine();
+                    f2();
+                }
+                catch
+                {
+                    Console.WriteLine("Некорректный ввод данных");
+                }
+            Console.ReadKey();
+
+
+}
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int n = Convert.ToInt32(textBox1.Text);
+                if (n <= 0) MessageBox.Show("Размер массива не может быть неположительным");
+                else
+                {
+                    double number = Convert.ToDouble(textBox2.Text);
+                    int[] arr = new int[n];
+                    string[] a = textBox3.Text.Split(' ');
+                    if (a.Length > n) MessageBox.Show("Вы ввели больше числе, чем необходиом, данные будут обрезаны!!!");
+                    for (int i = 0; i < n; i++)
+                    {
+                        if (Convert.ToInt32(a[i]) < number) arr[i] = Convert.ToInt32(a[i]) * 2;
+                        else arr[i] = Convert.ToInt32(a[i]);
+                    }
+
+                    textBox8.Text = "";
+                    for (int i = 0; i < n; i++)
+                    {
+                        textBox8.Text += arr[i];
+                        if (i < arr.Length - 1) textBox8.Text += (", ");
+                    }
+                }
+            }
+            catch { MessageBox.Show("Некорректный ввод данных"); }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int n = Convert.ToInt32(textBox4.Text);
+                int m = Convert.ToInt32(textBox5.Text);
+                if (n <= 0 || m <= 0) MessageBox.Show("Размер массива не может быть неположительным");
+                else
+                {
+                    double number = Convert.ToDouble(textBox6.Text);
+                    int[,] arr = new int[n, m];
+                    string[] a = textBox7.Text.Split('\n');
+                    if (a.Length > n) MessageBox.Show("Вы ввели больше числе, чем необходиом, данные будут обрезаны!!!");
+                    for (int i = 0; i < n; i++)
+                    {
+                        string[] b = a[i].Split(' ');
+                        for (int j = 0; j < m; j++)
+                        {
+                            if (b.Length > m) MessageBox.Show("Вы ввели больше числе, чем необходиом, данные будут обрезаны!!!");
+                            if (Convert.ToDouble(b[j]) < number) arr[i, j] = Convert.ToInt32(b[j]) * 2;
+                            else arr[i, j] = Convert.ToInt32(b[j]);
+                        }
+                    }
+                    for (int i = 0; i < n; i++)
+                    {
+                        for (int j = 0; j < m; j++) textBox9.Text += arr[i, j] + "  ";
+                        textBox9.Text += Environment.NewLine;
+                    }
+                }
+            }
+            catch { MessageBox.Show("Некорректный ввод данных"); }
+        }
+    }
+}
